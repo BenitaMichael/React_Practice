4bb8606c8f3491e0351b469d8eeba6c48fea3f5d
BenitaMichael <benubiaka@gmail.com>
2024-04-25 11:23:13 +0100

    set up for react hooks

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header';
+import Transactions from './Components/Transactions';
+
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+    <div>
+      <h1>REACT PRACTICE</h1>
+    </div>
+  );
+}
+
+export default App;
